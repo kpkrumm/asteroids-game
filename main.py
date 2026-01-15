590ef80133b71e2bbb641569e610df96d96211bc
kpkrumm <kpkrumm@gmail.com>
2026-01-14 19:22:40 -0600

    lock framerate, add circleshape, player

--- main.py
+++ main.py
@@ -7,13 +7,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             screen.fill("black")
-        pygame.display.flip()
-        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        pygame.display.flip()
+
+        clock.tick(60)
+        dt = (clock.tick(60))/1000
+
 if __name__ == "__main__":
     main()
